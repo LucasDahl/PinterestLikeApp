d6c61b69dd292ad515a9637cee77985f0561e362
Lucas Dahl <LucasDahl425@yahoo.com>
2018-12-12 13:00:11 -0800

    Added code for razerware to make a better looking collectionView.

--- PinterestLikeApp/ViewController.swift
+++ PinterestLikeApp/ViewController.swift
@@ -14,12 +14,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    // Properties
+    let images = [#imageLiteral(resourceName: "image-5"), #imageLiteral(resourceName: "image-7"), #imageLiteral(resourceName: "image-1"), #imageLiteral(resourceName: "image-6"), #imageLiteral(resourceName: "image-2"), #imageLiteral(resourceName: "image-4"), #imageLiteral(resourceName: "image-3")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set the delegate
+        if let layout = collectionView.collectionViewLayout as? PinterestLayout {
+            layout.delegate = self
+        }
+        
+        // Insets
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
     }
 
 
+} // End class
+
+//==============================================================
+// MARK: - Flow layout delegate (for customizing collectionView)
+//==============================================================
+
+extension ViewController: PinterestLayoutDelegate {
+    
+    // Would use this normally.
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        // Set up the grid
+////        let numberOfColumns: CGFloat = 2 // Chnage this fto change the number of columns displayed
+////        let width = collectionView.frame.size.width
+////        let xInsets: CGFloat = 10
+////        let cellSpacing: CGFloat = 5
+////        let image = images[indexPath.item]
+////        let height = image.size.height
+////
+////        return CGSize(width: (width / numberOfColumns) - (xInsets + cellSpacing), height: height)
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
+        
+        // Get the image height
+        let image = images[indexPath.item]
+        let height = image.size.height
+        
+        // Return height
+        return height
+        
+    }
+    
 }
 
 //====================
@@ -33,7 +77,7 @@ extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         // Return the number of cells
-        return 10
+        return images.count
         
     }
     
@@ -41,7 +85,13 @@ extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         // Create the cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        
+        // Get the image for the cell
+        let image = images[indexPath.row]
+        
+        // Set the cell's image
+        cell.imageView.image = image
         
         // Return the cell
         return cell
---
+++ PinterestLikeApp/Views/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  PinterestLikeApp
+//
+//  Created by Lucas Dahl on 12/12/18.
+//  Copyright © 2018 Lucas Dahl. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
