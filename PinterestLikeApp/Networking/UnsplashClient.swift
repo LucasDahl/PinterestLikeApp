342c43e98fe9a43565e40658121dd3c93992734c
Lucas Dahl <LucasDahl425@yahoo.com>
2018-12-16 13:42:39 -0800

    Took out some pointless CoreData code.

--- PinterestLikeApp/Networking/UnsplashClient.swift
+++ PinterestLikeApp/Networking/UnsplashClient.swift
@@ -12,7 +12,7 @@ class UnsplashClient: APIClient {
     
     // Static properties
     static let baseUrl = "https://api.unsplash.com"
-    static let apiKey = ""
+    static let apiKey = "7aeb6a376a44252921c9f75644753613fb77d5864a057947d57d89a65b579c1c"
     
     func fetch(with endpoint: UnsplashEndpoint, completion: @escaping (Either<Photos>) -> Void) {
         
